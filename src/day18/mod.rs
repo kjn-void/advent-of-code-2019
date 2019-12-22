29b2989c48f506dd58565f5fa075c2d91a8fa15f
Kenneth Jonsson <kjonsson@windriver.com>
2019-12-22 16:10:54 +0100

    Day 18

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -3,31 +3,32 @@ use super::Solution;
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 
-type Map = HashMap<Vec2D, char>;
-type KeyDistMatrix = Vec<Vec<KeyDistance>>;
+type Walls = HashSet<Vec2D>;
+type Map = HashMap<Vec2D, u32>;
+type KeyDistMatrix = HashMap<u32, Vec<KeyDistance>>;
 
 #[derive(Debug)]
-struct Keykeeper {
-    tile: char,     // current tile
+struct Robot {
+    at_key: u32,    // key the robot currently standing at
     have_keys: u32, // bitfield, bit 1 << (key - 'a') is set of key is collected
     steps: Distance,
 }
 
-impl Eq for Keykeeper {}
+impl Eq for Robot {}
 
-impl PartialEq for Keykeeper {
+impl PartialEq for Robot {
     fn eq(&self, other: &Self) -> bool {
         self.steps == other.steps
     }
 }
 
-impl Ord for Keykeeper {
+impl Ord for Robot {
     fn cmp(&self, other: &Self) -> Ordering {
         other.steps.cmp(&self.steps)
     }
 }
 
-impl PartialOrd for Keykeeper {
+impl PartialOrd for Robot {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(other.steps.cmp(&self.steps))
     }
@@ -35,16 +36,10 @@ impl PartialOrd for Keykeeper {
 
 #[derive(Debug)]
 struct KeyDistance {
-    to: char,
     steps: Distance,
-    keys: u32,  // bitfield, 1 << (door - 'A') is set if the key is along the path
+    to: u32,    // key at the end of this edge
+    keys: u32,  // bitfield, 1 << (key - 'a') is set if the key is along the path
     doors: u32, // bitfield, 1 << (door - 'A') is set if the door is along the path
-}
-
-fn all_keys(map: &Map) -> u32 {
-    map.values()
-        .filter(|&&tile| is_key(tile))
-        .fold(0, |keys, &tile| keys | key(tile))
 }
 
 fn has_item(item: u32, item_bag: u32) -> bool {
@@ -55,131 +50,105 @@ fn is_key(tile: char) -> bool {
     tile.is_ascii_lowercase()
 }
 
-fn key(key: char) -> u32 {
-    1 << key_idx(key) as u32
-}
-
-fn key_idx(key: char) -> usize {
-    (key as usize - 'a' as usize)
+fn key(tile: char) -> u32 {
+    1 << (tile as usize - 'a' as usize) as u32
 }
 
 fn is_door(tile: char) -> bool {
     tile.is_ascii_uppercase()
 }
 
-fn door(door: char) -> u32 {
-    1 << (door as u32 - 'A' as u32)
-}
-
-fn get_pos(map: &Map, what: char) -> Vec2D {
-    *map.iter()
-        .filter(|(_, &tile)| tile == what)
-        .next()
-        .unwrap()
-        .0
-}
-
-fn get_keys(map: &Map) -> Vec<char> {
-    map.values()
-        .filter(|&&tile| is_key(tile))
-        .map(|&key| key)
-        .collect()
+fn door(tile: char) -> u32 {
+    1 << (tile as u32 - 'A' as u32)
 }
 
 // Returns the distance from 'key' to all other keys and what doors are along
 // the route.
-fn distances(map: &Map, key_tile: char) -> Vec<KeyDistance> {
+fn distances(origin: Vec2D, walls: &Walls, keys: &Map, doors: &Map) -> Vec<KeyDistance> {
     let mut visited = HashSet::new();
     let mut queue = VecDeque::new();
-    let mut dists = Vec::new();
+    let mut edges = Vec::new();
 
-    queue.push_back((get_pos(map, key_tile), 0, 0, 0));
-    while let Some((pos, steps, keys, doors)) = queue.pop_front() {
+    queue.push_back((origin, 0, 0, 0));
+    while let Some((pos, steps, have_keys, seen_doors)) = queue.pop_front() {
         visited.insert(pos);
         let candidates = [UP, DOWN, LEFT, RIGHT]
             .iter()
-            .map(|&dir| {
-                let new_pos = dir + pos;
-                (new_pos, map.get(&new_pos).unwrap())
-            })
-            .filter(|(new_pos, &tile)| tile != '#' && !visited.contains(new_pos));
-        for (new_pos, &tile) in candidates {
-            let is_key = is_key(tile);
-            let new_keys = keys | if is_key { key(tile) } else { 0 };
-            let new_doors = doors | if is_door(tile) { door(tile) } else { 0 };
+            .map(|&dir| pos + dir)
+            .filter(|new_pos| !visited.contains(new_pos) && !walls.contains(new_pos));
+        for new_pos in candidates {
+            let new_key = *keys.get(&new_pos).unwrap_or(&0);
             let new_steps = steps + 1;
-
-            if is_key {
-                dists.push(KeyDistance {
-                    to: tile,
+            if new_key != 0 {
+                edges.push(KeyDistance {
+                    to: new_key,
                     steps: new_steps,
-                    keys: keys,
-                    doors: doors,
+                    keys: have_keys,
+                    doors: seen_doors,
                 });
             }
-            queue.push_back((new_pos, new_steps, new_keys, new_doors))
+            queue.push_back((
+                new_pos,
+                new_steps,
+                have_keys | new_key,
+                seen_doors | *doors.get(&new_pos).unwrap_or(&0),
+            ))
         }
     }
-    dists
+    edges
 }
 
-fn push_reachables(pq: &mut BinaryHeap<Keykeeper>, kk: &Keykeeper, dists: &KeyDistMatrix) {
-    for dist in &dists[key_idx(kk.tile)] {
-        let new_key = key(dist.to);
-        let visited = has_item(new_key, kk.have_keys);
-        let can_unlock_doors = (!kk.have_keys & dist.doors) == 0;
-        let all_keys_collected = (!kk.have_keys & dist.keys) == 0;
-        let new_steps = dist.steps + kk.steps;
+fn dist_matrix(walls: &Walls, keys: &Map, doors: &Map) -> KeyDistMatrix {
+    let mut hm = KeyDistMatrix::new();
+    for (&pos, &key) in keys {
+        hm.insert(key, distances(pos, walls, keys, doors));
+    }
+    hm
+}
 
-        // Not visited, is currently reachable and already collected all keys
-        // along the path
+fn push_reachables(pqueue: &mut BinaryHeap<Robot>, robot: &Robot, edges: &KeyDistMatrix) {
+    for edge in edges.get(&robot.at_key).unwrap() {
+        let visited = has_item(edge.to, robot.have_keys);
+        let can_unlock_doors = (!robot.have_keys & edge.doors) == 0;
+        let all_keys_collected = (!robot.have_keys & edge.keys) == 0;
+        let new_steps = edge.steps + robot.steps;
+
         if !visited && can_unlock_doors && all_keys_collected {
-            pq.push(Keykeeper {
-                tile: dist.to,
-                have_keys: kk.have_keys | new_key,
+            pqueue.push(Robot {
+                at_key: edge.to,
+                have_keys: robot.have_keys | edge.to,
                 steps: new_steps,
             });
         }
     }
 }
 
-fn dist_matrix(map: &Map) -> KeyDistMatrix {
-    let mut m = Vec::new();
-    for _ in 0..26 {
-        m.push(Vec::new());
-    }
-    for &key_tile in &get_keys(map) {
-        m[key_idx(key_tile)] = distances(map, key_tile);
-    }
-    m
-}
-
-fn collect_keys(map: &Map) -> Distance {
-    let all_keys = all_keys(map);
-    let dists = dist_matrix(map);
-    let mut pq = BinaryHeap::new();
+fn collect_keys(start_pos: Vec2D, walls: &Walls, keys: &Map, doors: &Map) -> Distance {
+    let all_keys = keys.values().fold(0, |keys, &key| keys | key);
+    let dists = dist_matrix(walls, keys, doors);
+    let mut pqueue = BinaryHeap::new();
     let mut duplicate_route = HashSet::new();
 
     // Add all keys reachable from the start position
-    for dist in distances(map, '@') {
-        if is_key(dist.to) && dist.doors == 0 && dist.keys == 0 {
-            pq.push(Keykeeper {
-                tile: dist.to,
-                have_keys: key(dist.to),
-                steps: dist.steps,
+    for edge in distances(start_pos, walls, keys, doors) {
+        if edge.doors == 0 && edge.keys == 0 {
+            pqueue.push(Robot {
+                at_key: edge.to,
+                have_keys: edge.to,
+                steps: edge.steps,
             });
         }
     }
 
     let mut min_steps = std::u32::MAX;
-    while let Some(kk) = pq.pop() {
-        if kk.steps > min_steps {
-            break
+    while let Some(robot) = pqueue.pop() {
+        if robot.steps > min_steps {
+            break;
         }
-        if kk.have_keys == all_keys {
-            min_steps = kk.steps;
-        } else if duplicate_route.insert((kk.tile, kk.have_keys)) {
-            push_reachables(&mut pq, &kk, &dists);
+        if robot.have_keys == all_keys {
+            min_steps = robot.steps;
+        } else if duplicate_route.insert((robot.at_key, robot.have_keys)) {
+            push_reachables(&mut pqueue, &robot, &dists);
         }
     }
     min_steps
@@ -187,27 +156,77 @@ fn collect_keys(map: &Map) -> Distance {
 
 impl Solution for State {
     fn part1(&self) -> String {
-        collect_keys(&self.map).to_string()
+        collect_keys(self.start_pos, &self.walls, &self.keys, &self.doors).to_string()
     }
 
     fn part2(&self) -> String {
-        "".to_string()
+        let mut tot_distance = 0;
+        // Shift map so that the original start position is at (0, 0), one
+        // submap per quadrant
+        let mut walls = self
+            .walls
+            .iter()
+            .clone()
+            .map(|&pos| pos - self.start_pos)
+            .collect::<Walls>();
+        walls.extend(&[UP, DOWN, LEFT, RIGHT]);
+
+        for &start_pos in &[UP + LEFT, UP + RIGHT, DOWN + LEFT, DOWN + RIGHT] {
+            let keys = self
+                .keys
+                .clone()
+                .into_iter()
+                .map(|(pos, key)| (pos - self.start_pos, key))
+                .filter(|(pos, _)| pos.x() * start_pos.x() > 0 && pos.y() * start_pos.y() > 0)
+                .collect::<Map>();
+            let doors = self
+                .doors
+                .clone()
+                .into_iter()
+                .map(|(pos, door)| (pos - self.start_pos, door))
+                .filter(|(pos, _)| pos.x() * start_pos.x() > 0 && pos.y() * start_pos.y() > 0)
+                .filter(|(_, door)| keys.values().find(|&key| key == door) != None)
+                .collect::<Map>();
+            tot_distance += collect_keys(start_pos, &walls, &keys, &doors);
+        }
+        tot_distance.to_string()
     }
 }
 
 // State required to solve day 18
 pub struct State {
-    map: Map,
+    start_pos: Vec2D,
+    walls: Walls,
+    keys: Map,
+    doors: Map,
 }
 
 pub fn solution(lines: Vec<&str>) -> Box<dyn Solution> {
-    let mut map = Map::new();
-    for y in 0..lines.len() {
-        for (x, ch) in lines[y].chars().enumerate() {
-            map.insert(Vec2D::from(x as Coord, y as Coord), ch);
+    let mut keys = Map::new();
+    let mut doors = Map::new();
+    let mut walls = Walls::new();
+    let mut start_pos = None;
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, tile) in line.chars().enumerate() {
+            let pos = Vec2D::from(x as Coord, y as Coord);
+            if tile == '@' {
+                start_pos = Some(pos);
+            } else if tile == '#' {
+                walls.insert(pos);
+            } else if is_key(tile) {
+                keys.insert(pos, key(tile));
+            } else if is_door(tile) {
+                doors.insert(pos, door(tile));
+            }
         }
     }
-    Box::new(State { map: map })
+    Box::new(State {
+        start_pos: start_pos.unwrap(),
+        walls: walls,
+        keys: keys,
+        doors: doors,
+    })
 }
 
 #[cfg(test)]
